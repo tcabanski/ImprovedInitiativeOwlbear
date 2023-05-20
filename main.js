4e4e9a76a59196097bb29df7e2d6253b765c7a49
Tom Cabanski <tom@cabanski.com>
2023-05-20 18:50:41 -0500

    Basic display working

--- main.js
+++ main.js
@@ -6,50 +6,55 @@ import axios from 'axios'
 import { io } from 'socket.io-client'
 
 axios.get('http://localhost:3000').then(response => {
-  console.log(response.data);
-  var html = '';
-  response.data.forEach(combatant => {
-    html += '<div>'
-    if (combatant.isCurrent) {
-      html += '!!'
-    }
-  html += `${combatant.init} ${combatant.name} ${combatant.hp}</div>`
-    });
-  document.querySelector('#app').innerHTML = html;
+  displayCombatants(response.data);
 }).catch(error => console.error(error));
 
 var socket = io('http://localhost:3000');
 socket.on('combatants', function (message) {
-  console.log(message.combatants);
-  var html = '';
-  message.combatants.forEach(combatant => {
-    html += '<div>'
-    if (combatant.isCurrent) {
-      html += '!!'
-    }
-  html += `${combatant.init} ${combatant.name} ${combatant.hp}</div>`
-    });
-  document.querySelector('#app').innerHTML = html;
+  displayCombatants(message.combatants);
 });
 
-/*
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+function displayCombatants(combatants) {
+  console.log(combatants);
+  var html = `
+  <div class="row">
+    <div class="col-2">
+      <h3 class='float-start'>Init</h3>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    <div class="col-6">
+      <h3 class='float-start'>Name</h3>
+    </div>
+    <div class="col">
+      <h3 class='float-start'>Health</h3>
+    </div>
+  </div>`;
+  combatants.forEach(combatant => {
+    if (combatant.isCurrent) {
+      html += '<div class="row bg-primary">'
+    } else {
+      html += '<div class="row">'
+    }
+    var healthIndicator = 'unknown';
+    switch (combatant.hp) {
+      case 'Healthy':
+        healthIndicator = '<img src="/public/greenheart.svg" height="20" width="20" alt="Healthy" />';
+        break;
+      case 'Hurt':
+        healthIndicator = '<img src="/public/yellowheart.svg" height="20" width="20" alt="Hurt" />';
+        break;
+      case 'Bloodied':
+        healthIndicator = '<img src="/public/redheart.svg" height="20" width="20" alt="Bloodied" />';
+        break;
+      case 'Defeated':
+        healthIndicator = '<img src="/public/skull.svg" height="20" width="20" alt="Dead" />';
+        break;
+      default:
+        healthIndicator = '---';
+        break;
 
-setupCounter(document.querySelector('#counter'))
-*/
+    }
+    html += `<div class="col-2"><span class='float-start'>${combatant.init}</span></div><div class="col-6">
+      <span class='float-start'>${combatant.name}</div></span><div class="col"><span class='float-start'>${healthIndicator}</span></div></div>`
+  });
+  document.querySelector('#myApp').innerHTML = html;
+}
